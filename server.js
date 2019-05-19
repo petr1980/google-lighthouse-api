0196687d17980e23b78c7e8680e05634359ba3ce
Peter <goszak10@gmail.com>
2019-05-19 21:50:55 +0300

    bugfix2

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use(express.static(__dirname + '/dist/'))
+app.use(express.static(__dirname + "/dist/"))
+app.get(/.*/, function (req, res) {
+  res.sendfile(__dirname + "/dist/index.html")
+});
+app.listen(port);
 
-app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html")
-})
-app.listen(port)
-
-console.log('Server run ' + port)
+console.log('Server started: ' + port);
